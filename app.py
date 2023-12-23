9e66e0b8b2b56bc216f431be2c07daf6c5c57b5a
peunsu <44690893+peunsu@users.noreply.github.com>
2023-12-23 11:33:45 +0000

    Change reply message format, add logger and config.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
+import json
 import logging
 from functools import partial
+import datetime
 import discord
 import deepl
 
@@ -11,6 +13,9 @@ logger = logging.getLogger(discord.client.__name__)
 
 class TranslatorApp(discord.Client):
     async def on_ready(self):
+        self.config = json.load(open("config.json", "r"))
+        logger.info(f"Translate messages sent in the following channels: {self.config['channels']}")
+        
         auth_key = os.environ["DEEPL_KEY"]
         translator = deepl.Translator(auth_key)
         self.translate = partial(translator.translate_text, target_lang=os.environ["TARGET_LANG"])
@@ -19,6 +24,9 @@ class TranslatorApp(discord.Client):
 
     async def on_message(self, message):
         if message.author == self.user:
+            return
+        
+        if message.channel.id not in self.config["channels"]:
             return
         
         text_len = 0
@@ -31,23 +39,45 @@ class TranslatorApp(discord.Client):
                 
                 for key in embed_dict.keys():
                     if key in ["title", "description"]:
-                        msg_len += len(embed_dict[key])
+                        text_len += len(embed_dict[key])
                         embed_dict[key] = self.translate(embed_dict[key]).text
                         
                     elif key == "fields":
                         for field in embed_dict["fields"]:
                             for attr in ["value", "name"]:
-                                msg_len += len(field[attr])
+                                text_len += len(field[attr])
                                 field[attr] = self.translate(field[attr]).text
                                 
+                embed_dict["author"] = {
+                    "name": message.author.name,
+                    "icon_url": message.author.avatar.url
+                }
+                embed_dict["footer"] = {
+                    "text": "Translated by DeepL"
+                }
+                embed_dict["timestamp"] = datetime.datetime.now().isoformat()
+                embed_dict["color"] = discord.Color.blue().value
+                                
                 reply_embeds.append(discord.Embed.from_dict(embed_dict))
-            
-            logger.info(f"Translate embed message sent by {message.author} (Text length: {text_len})")
-            return await message.reply(embeds=reply_embeds)
+                
+                logger.info(f"Translate embed message sent by {message.author} (Text length: {text_len})")
+                return await message.reply(embeds=reply_embeds)
         
         text_len += len(message.content)
+        
         logger.info(f"Translate normal message sent by {message.author} (Text length: {text_len})")
-        await message.reply(self.translate(message.content).text)
+        await message.reply(embed=discord.Embed.from_dict({
+            "author": {
+                "name": message.author.name,
+                "icon_url": message.author.avatar.url
+            },
+            "description": self.translate(message.content).text,
+            "footer": {
+                "text": "Translated by DeepL"
+            },
+            "timestamp": datetime.datetime.now().isoformat(),
+            "color": discord.Color.blue().value
+        }))
 
 intents = discord.Intents.default()
 intents.message_content = True
