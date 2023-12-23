45382c8cf445c49c3f2cd34e30e25734e79249c7
peunsu <44690893+peunsu@users.noreply.github.com>
2023-12-23 10:21:44 +0000

    Added logger to print the length of the original text.

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import os
-from functools import partial
 from dotenv import load_dotenv
+import logging
+from functools import partial
 import discord
 import deepl
 
 load_dotenv()
 
+logger = logging.getLogger(discord.client.__name__)
+
 class TranslatorApp(discord.Client):
     async def on_ready(self):
-        auth_key = os.environ["auth_key"]
+        auth_key = os.environ["DEEPL_KEY"]
         translator = deepl.Translator(auth_key)
-        self.translate = partial(translator.translate_text, target_lang=os.environ["target_lang"])
+        self.translate = partial(translator.translate_text, target_lang=os.environ["TARGET_LANG"])
         
-        print(f'Logged on as {self.user}!')
+        logger.info(f'Logged on as {self.user}.')
 
     async def on_message(self, message):
         if message.author == self.user:
             return
+        
+        text_len = 0
         
         if embeds := message.embeds:
             reply_embeds = []
@@ -26,19 +31,26 @@ class TranslatorApp(discord.Client):
                 
                 for key in embed_dict.keys():
                     if key in ["title", "description"]:
+                        msg_len += len(embed_dict[key])
                         embed_dict[key] = self.translate(embed_dict[key]).text
+                        
                     elif key == "fields":
                         for field in embed_dict["fields"]:
                             for attr in ["value", "name"]:
+                                msg_len += len(field[attr])
                                 field[attr] = self.translate(field[attr]).text
+                                
                 reply_embeds.append(discord.Embed.from_dict(embed_dict))
-                
+            
+            logger.info(f"Translate embed message sent by {message.author} (Text length: {text_len})")
             return await message.reply(embeds=reply_embeds)
         
+        text_len += len(message.content)
+        logger.info(f"Translate normal message sent by {message.author} (Text length: {text_len})")
         await message.reply(self.translate(message.content).text)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = TranslatorApp(intents=intents)
-client.run(os.environ["token"])
+client.run(os.environ["BOT_TOKEN"])
--- test.py
+++
@@ -1,6 +0,0 @@
-print(len("""
-OptiFine 1.20.4_HD_U_I7_pre2
-https://optifine.net/downloads (click on "Preview versions")
-- not compatible with Forge
-@Releases 🔔
-          """))
